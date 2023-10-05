01b71f9381201aaed13f2117d46efc4a00a34890
Jaguar <gitibillyzombie@gmail.com>
2023-10-05 02:45:18 -0700

    meow

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
 
     from magic_calculation_102 import add, sub
     if a < b:
-        sum = add(a,b)
+        sum = add(a, b)
 
         for i in range(4, 6):
             sum = add(sum, i)
