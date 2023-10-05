12cbcbcad91dd9a0fd193f77b4ded5a64dd8f077
Jaguar <gitibillyzombie@gmail.com>
2023-10-05 02:25:45 -0700

    meow

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     sys.argv.pop(0)
     length = len(sys.argv)
 
-    if(length == 0):
+    if length == 0:
         print("0 arguments.")
-    elif (length == 1):
+    elif length == 1:
         print("1 argument:")
         print("{:d}: {}".format(len(sys.argv), sys.argv[0]))
     else:
         print("{:d} arguments:".format(length))
         number = 1
         for argument in sys.argv:
-            print("{:d}: {}".format(number,argument))
-            number +=1
+            print("{:d}: {}".format(number, argument))
+            number += 1
