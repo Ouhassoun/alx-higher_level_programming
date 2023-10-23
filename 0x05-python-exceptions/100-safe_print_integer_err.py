cda9954379a736ba9d276d00eaec8664ccd82249
Jaguar <gitibillyzombie@gmail.com>
2023-10-23 15:57:27 +0000

    meow

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
+    from sys import stderr
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+        return True
+    except Exception as err:
+        print("Exception: {}".format(err), file=stderr)
+        return False
