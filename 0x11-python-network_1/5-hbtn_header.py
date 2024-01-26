4c801fa84c6595cedd6fd13ac2e79f6bddb668d8
Jaguar <gitibillyzombie@gmail.com>
2024-01-26 22:59:09 +0000

    meow

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,8 @@ sends request to url and display X-Request-Id header value
 import requests
 import sys
 
+if __name__ == '__main__':
+    url = sys.argv[1]
 
-url = sys.argv[1]
-response = requests.get(url)
-print(response.headers.get("X-Request-Id"))
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
