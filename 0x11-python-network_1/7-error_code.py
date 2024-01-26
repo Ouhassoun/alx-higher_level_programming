652bb2e776140fe0d17e1a9af79e47f892804005
Jaguar <gitibillyzombie@gmail.com>
2024-01-26 23:02:10 +0000

    meow

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-displays body of response and handles erros
-"""
+""" script that takes in a URL, sends a request to
+ the URL and displays the body of the response."""
 import sys
+
 import requests
 
-
 if __name__ == "__main__":
-    url = sys.agrv[1]
-    request = requests.get(url)
-    if request.status_code >= 400:
-        print("Error code: {}".format(request.status_code))
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(request.text)
+        print(r.text)
